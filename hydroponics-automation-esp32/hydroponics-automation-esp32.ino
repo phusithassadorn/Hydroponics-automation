6f2f5e379a161ebcd1a29101adf694acc9c89fb6
Phusit Hassadorn <68945774+phusithassadorn@users.noreply.github.com>
2022-04-02 19:25:11 +0700

    Update hydroponics-automation-esp32.ino

--- hydroponics-automation-esp32/hydroponics-automation-esp32.ino
+++ hydroponics-automation-esp32/hydroponics-automation-esp32.ino
@@ -1,12 +1,31 @@
-#define ONBOARD_LED  2
 
-void setup() {
-  pinMode(ONBOARD_LED,OUTPUT);
+#define BLYNK_PRINT Serial
+#define RELAY_PIN 14
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+
+char auth[] = "YourAuthToken";
+char ssid[] = "Hassadorn_5G";
+char pass[] = "0998394346";
+
+void relaysOutput (){
+   pinMode(RELAY_PIN, OUTPUT);
+  }
+
+void setup()
+
+ 
+{
+
+Serial.begin(115200);
+
+Blynk.begin(auth, ssid, pass);
 }
 
-void loop() {
-  delay(1000);
-  digitalWrite(ONBOARD_LED,HIGH);
-  delay(100);
-  digitalWrite(ONBOARD_LED,LOW);
+void loop()
+{
+Blynk.run();
 }
